c1af0263c053c70dbfb1af0839138bb8b9f36d03
jd5688 <jd5688@gmail.com>
2016-04-16 11:31:58 +1200

    indicate when user is typing and display user name in the message

--- index.js
+++ index.js
@@ -2,18 +2,40 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var url = require('url');
+var url_parts;
 
 app.use(express.static("css"));
 
 http.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
 
 app.get('/', function (req, res) {
-  res.sendfile('view/index.html');
+	res.sendfile('view/index.html');
 });
 
+app.get('/getUser', function (req, res) {
+	url_parts = url.parse(req.url, true);
+	res.end(url_parts.query.id);
+})
+
 io.on('connection', function(socket) {
-	socket.emit('news', { hello: 'world' });
 	console.log('a user connected');
+
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
+	});
+
+	socket.on('chat message', function(msg){
+	    io.emit('chat message', msg);
+	});
+
+	socket.on('typing a message', function(data) {
+		io.emit('typing a message', data);
+	});
+
+	socket.on('log user', function (user) {
+		io.emit('log user', user);
+	})
 });
