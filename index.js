e593987a9d0d4dd042761f5f4ce2ec699c223ee2
jd5688 <jd5688@gmail.com>
2016-04-16 15:41:52 +1200

    add ejs templating

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var url = require('url');
+var ejs = require('ejs');
 var url_parts;
 
+ejs.open = '{{'; 
+ejs.close = '}}';
+
+app.set('strict routing', true);
+
+// redirect if with trailing slash
+app.use(function(req, res, next) {
+   if(req.url.substr(-1) == '/' && req.url.length > 1)
+       res.redirect(301, req.url.slice(0, -1));
+   else
+       next();
+});
 app.use(express.static("css"));
+app.set('view engine', 'ejs');
 
 http.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
 
 app.get('/', function (req, res) {
-	res.sendfile('view/index.html');
+	//res.sendfile(__dirname + '/view/index.ejs');
+	res.render(__dirname + '/view/index');
+});
+
+app.get('/chat', function (req, res) {
+	// sendFile (capital F) does not work with socket.io
+	//res.sendfile(__dirname + '/view/chat.ejs');
+	res.render(__dirname + '/view/chat');
 });
 
 app.get('/getUser', function (req, res) {
